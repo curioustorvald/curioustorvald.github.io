deefa0d90d14a2a837d0ff2d82e03ed73b9af9ff
minjaesong <alswo9628@gmail.com>
2022-02-03 21:55:07 +0900

    techtales wip

--- techtales/blog.js
+++ techtales/blog.js
@@ -2,10 +2,6 @@ function siteheaderinit() {
     document.getElementsByTagName("siteheader")[0].innerHTML = `<h1 class="site_header">Torvald's Tech Tales</h1>`
 }
 
-function panelnavinit() {
-    
-}
-
 function pageinit() {
     
 }
